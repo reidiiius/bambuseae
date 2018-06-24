0b06ab32bf148e685703dd88b995bd31738c79f0
Reid Netterville III <reidiiius@gmail.com>
2018-06-24 15:56:56 -0500

    180624.0

--- scripts/bamboo.js
+++ scripts/bamboo.js
@@ -1,10 +1,96 @@
-/* Copyright (C) 2018 Reid Netterville III */
+/* Copyright (c) 2018 Reid Netterville III */
 
 "use strict";
 
 var HuangShan = {};
 
-HuangShan.JinshuLong = {
+HuangShan.WhiteTiger = {
+      j2: "vv zq __ __ ry wu __ uw __ sx __ qz ",
+      j3: "vt __ tv xq __ ws __ uu __ sw __ qx ",
+      j5: "wr __ ut __ sv oq qo __ __ tu __ rw ",
+      j6: "vu __ tw __ rx wt __ uv yq __ __ qy ",
+      k1: "__ qx vt __ tv xq __ ws __ uu __ sw ",
+      k2: "yr __ __ qz vv zq __ xs __ wu __ uw ",
+      k5: "xr __ wt __ uv yq __ __ qy vu __ tw ",
+      k6: "vs __ tu __ rw wr __ ut __ __ oq qo ",
+      n0: "vr __ tt __ rv wq __ us __ su __ qw ",
+     j17: "__ __ tu __ rw wr __ ut __ sv oq qo ",
+     j23: "vv zq __ xs __ wu __ uw __ sx __ qz ",
+     j25: "qr vp __ __ pv rq wo __ __ yu __ ow ",
+     j26: "vv zq __ __ ry wu __ uw yr __ __ qz ",
+     j36: "vu __ tw xr __ wt __ uv yq __ __ qy ",
+     j56: "xr __ wt __ uv yq so __ qy __ __ tw ",
+     k12: "__ sx __ qz vv zq __ xs __ wu __ uw ",
+     k15: "__ rx wt __ uv yq __ __ qy vu __ tw ",
+     k25: "yr __ __ qz vv zq __ __ ry wu __ uw ",
+     k26: "vp __ __ pv rq wo __ uy __ __ ow qr ",
+     k34: "wr __ ut __ __ oq qo vs __ tu __ rw ",
+     k56: "vu __ tw __ rx wt __ __ yq __ os qy ",
+    j236: "vv zq __ xs __ wu __ uw yr __ __ qz ",
+    j256: "qr vp __ __ pv rq wo __ uy __ __ ow ",
+    j2k5: "qr vp __ __ pv rq __ __ uy yu __ ow ",
+    j2k6: "vv zq __ __ ry wu __ uw __ __ ot qz ",
+    j2y3: "vv zq to __ __ wu __ uw __ sx __ qz ",
+    j3k5: "yr __ ot qz __ zq __ __ ry wu __ uw ",
+    j3k6: "vo __ ty xu __ ww __ ux __ __ ov qq ",
+    j5y6: "wr __ ut __ sv oq qo vs __ __ __ rw ",
+    k125: "__ sx __ qz vv zq __ __ ry wu __ uw ",
+    k1j5: "__ ux yt __ ov qq vo __ __ xu __ ww ",
+    k1j6: "__ zq to __ ry wu __ uw yr __ __ qz ",
+    k256: "vp __ __ pv rq wo __ __ yu __ ow qr ",
+    k2j5: "yr __ __ qz vv zq to __ __ wu __ uw ",
+    k2j6: "vp __ __ pv rq wo __ uy yu __ __ qr ",
+    k2x1: "__ __ ot qz vv zq __ xs __ wu __ uw ",
+    k6x5: "vs __ tu __ rw wr __ __ __ sv oq qo ",
+    n167: "yr __ __ qz __ zq to __ ry wu __ uw ",
+    n345: "__ zq __ __ ry wu __ uw yr __ ot qz ",
+    n5y2: "vp __ __ pv rq __ __ uy yu __ ow qr ",
+    n6x2: "qr vp __ __ pv rq wo __ uy yu __ __ ",
+   j17y2: "vs __ __ __ rw wr __ ut __ sv oq qo ",
+   j23k6: "vv zq __ xs __ wu __ uw __ __ ot qz ",
+   j25y6: "or qp __ __ tv pq ro ws __ __ __ sw ",
+   j26y3: "vv zq to __ __ wu __ uw yr __ __ qz ",
+   j2k34: "or qp __ __ __ pq ro ws __ uu __ sw ",
+   j2k56: "vv zq __ __ ry wu __ __ yr __ ot qz ",
+   j34k6: "vt __ tv pq ro __ __ uu __ __ or qp ",
+   j56y7: "xr __ wt __ uv yq so __ qy vu __ __ ",
+   k12j5: "__ sx __ qz vv zq to __ __ wu __ uw ",
+   k17j5: "or qp vt __ tv pq ro __ __ uu __ __ ",
+   k2j17: "__ __ __ pq ro ws __ uu __ sw or qp ",
+   k25x1: "__ __ ot qz vv zq __ __ ry wu __ uw ",
+   k26x5: "vt __ __ pq ro ws __ __ __ sw or qp ",
+   k2j56: "yr __ __ qz vv zq to __ ry __ __ uw ",
+   k34x2: "wr __ __ __ sv oq qo vs __ tu __ rw ",
+   k56x4: "vu __ tw __ rx __ __ uv yq __ os qy ",
+   n25x6: "or qp vt __ __ pq ro ws __ __ __ sw ",
+   n26y5: "__ __ tv pq ro ws __ __ __ sw or qp ",
+   n45y2: "vo __ __ xu __ ww __ ux yt __ ov qq ",
+   n67x2: "__ ux __ __ ov qq vo __ ty xu __ ww ",
+  j136y7: "__ __ tw xr __ wt __ uv yq so __ qy ",
+  j167y2: "vu __ __ __ rx wt __ uv yq __ os qy ",
+  j246y3: "vv zq to __ ry __ __ uw yr __ __ qz ",
+  j26y34: "vv zq to xs __ __ __ uw yr __ __ qz ",
+  j2k6x5: "vv zq __ __ ry wu __ __ __ sx ot qz ",
+  j2k6y3: "vv zq to __ __ wu __ uw __ __ ot qz ",
+  j346y5: "yr __ ot qz vv zq __ __ ry __ __ uw ",
+  j3k5x4: "vu __ tw xr __ __ __ uv yq so __ qy ",
+  k135x4: "__ rx wt __ __ yq __ os qy vu __ tw ",
+  k157x6: "vv zq to __ ry wu __ __ yr __ __ qz ",
+  k1j6y7: "__ rx wt __ uv yq __ os qy vu __ __ ",
+  k257x1: "yr __ ot qz vv zq __ __ ry wu __ __ ",
+  k25x17: "__ sx ot qz vv zq __ __ ry wu __ __ ",
+  k2j5x1: "__ __ ot qz vv zq to __ __ wu __ uw ",
+  k2j5y6: "yr __ __ qz vv zq to xs __ __ __ uw ",
+  k345x2: "xr __ __ __ uv yq so __ qy vu __ tw ",
+  n167x4: "vu __ __ xr __ wt __ uv yq so __ qy ",
+  n345y7: "__ rx __ __ uv yq __ os qy vu __ tw ",
+ j2k56x4: "vv zq __ __ ry __ __ uw yr __ ot qz ",
+ j3k56x4: "vo __ ty xu __ __ __ ux yt __ ov qq ",
+ k1j56y7: "__ ux yt __ ov qq vo __ ty xu __ __ ",
+ k2j56y7: "yr __ __ qz vv zq to __ ry wu __ __ "};
+
+
+HuangShan.AzureDragon = {
       j2: "汞汞 钚铁 一一 一一 铜镎 铅金 一一 金铅 一一 银铀 一一 铁钚 ",
       j3: "汞锡 一一 锡汞 铀铁 一一 铅银 一一 金金 一一 银铅 一一 铁铀 ",
       j5: "铅铜 一一 金锡 一一 银汞 钒铁 铁钒 一一 一一 锡金 一一 铜铅 ",
@@ -101,14 +187,14 @@ if (document.title === "") {
 HuangShan.clade = document.title.split("_");
 
 if (HuangShan.clade[0] !== undefined && HuangShan.clade[0] !== "" &&
-    HuangShan.JinshuLong.hasOwnProperty(HuangShan.clade[0])) {
+    HuangShan.AzureDragon.hasOwnProperty(HuangShan.clade[0])) {
   HuangShan.prime = HuangShan.clade[0];
 } else {
   HuangShan.prime = "n0";
 }
 
 if (HuangShan.clade[1] !== undefined && HuangShan.clade[1] !== "" &&
-    HuangShan.JinshuLong.hasOwnProperty(HuangShan.clade[1])) {
+    HuangShan.AzureDragon.hasOwnProperty(HuangShan.clade[1])) {
   HuangShan.retro = HuangShan.clade[1];
 } else {
   HuangShan.retro = "n0";
